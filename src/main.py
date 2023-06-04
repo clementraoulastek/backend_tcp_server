cb1c9af10eea004e75db8760076b0694519f3470
clement raoul astek <craoul@groupeastek.fr>
2023-06-04 17:39:09 +0200

    first test

--- src/main.py
+++ src/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Client(BaseModel):
+    login: str
+    password: str
+    
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.post("/login/{login}")
+def login(client: Client):
+    return {"login": client.login, "password": client.password}
